aa0a68b776af9ea754e19a00da569a52f8c6048e
nico-verna <nicholasverna4@gmail.com>
2023-11-12 14:51:02 -0500

    Updated to version 2.0

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # cwtd-photo-gallery
 Coding with The Doctors Raspberry Pi 3 Touchscreen Photo Gallery
 This is code used for the Coding With the Doctors club in Palm Beach State college that will function on a Raspberry Pi 3 touch screen module.
-Features include a title screen, a start button, and 6 photos of the club.
-More features to come should the ideas allow it.
+Features include:
+Title screen
+Start button
+Top secret button (2.0)
+Get to know us button (2.0)
+Doomsday button (2.0)
+6 pictures of club activity 
+
+Version 2.0 created the expansion into the top secret area, with more to come soon.
+Current ideas for expansion:
+Informational Branch (Why our club is good)
--- photos.py
+++ photos.py
@@ -3,19 +3,38 @@ import pygame
 import pygame_gui
 import sys
 from pygame.locals import *
+import random
 
 # Initialize Pygame
 pygame.init()
 
 # Set up some constants
 WIDTH, HEIGHT = 800, 480  # Adjust to your screen's resolution
-IMAGE_DIR = "Digital Photo Gallery\Photos"  # Replace with your images directory
+IMAGE_DIR = "Digital Photo Gallery/Photos"  # Replace with your images directory
 
 # Set up the display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Create a UIManager for pygame_gui
 manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+# Buttons
+top_secret_button = None
+know_button = None
+return_button = None
+# Global variables
+doomsday_running = False
+
+def draw_main():
+    global top_secret_button, know_button
+    # Create the "TOP SECRET" button
+    top_secret_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 10), (150, 50)),
+                                                 text='TOP SECRET',
+                                                 manager=manager)
+    # Create the "Get to know us!" button
+    know_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH - 160, 10), (150, 50)),
+                                                     text='Get to know us!',
+                                                     manager=manager)
+
 
 # Define the fade-in and fade-out function
 def fade_transition(old_image, new_image):
@@ -43,16 +62,6 @@ def show_title_screen():
     text = font.render("Coding With the Doctors Photo Gallery", True, (255, 255, 255))  # Create the title text
     screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))  # Draw the title text
     pygame.display.flip()  # Update the display
-
-# Function to handle touch events
-def handle_event(event):
-    if event.type == QUIT:
-        pygame.quit()
-    elif event.type == MOUSEBUTTONDOWN:
-        if event.button == 1:  # Left click
-            return 'next'
-        elif event.button == 3:  # Right click
-            return 'prev'
 
 # Load images
 images = []
@@ -95,22 +104,183 @@ while not start_gallery:
     pygame.display.update()
     pygame.display.flip()  # Ensure the screen updates are displayed
 
+# Define a theme dictionary
+theme = {
+    "dark": {
+        "font": {
+            "name": "fira_code",
+            "size": 14,
+            "bold": False,
+            "italic": False
+        },
+        "colours": {
+            "dark_bg": pygame.Color("#282828"),
+            "normal_bg": pygame.Color("#3c3836"),
+            "hovered_bg": pygame.Color("#504945"),
+            "disabled_bg": pygame.Color("#282828"),
+            "selected_bg": pygame.Color("#689d6a"),
+            "active_bg": pygame.Color("#98971a"),
+            "dark_text": pygame.Color("#a89984"),
+            "normal_text": pygame.Color("#ebdbb2"),
+            "hovered_text": pygame.Color("#ebdbb2"),
+            "disabled_text": pygame.Color("#a89984"),
+            "selected_text": pygame.Color("#ebdbb2"),
+            "active_text": pygame.Color("#ebdbb2"),
+            "dark_icon": pygame.Color("#a89984"),
+            "normal_icon": pygame.Color("#ebdbb2"),
+            "hovered_icon": pygame.Color("#ebdbb2"),
+            "disabled_icon": pygame.Color("#a89984"),
+            "selected_icon": pygame.Color("#ebdbb2"),
+            "active_icon": pygame.Color("#ebdbb2"),
+            "dark_outline": pygame.Color("#a89984"),
+            "normal_outline": pygame.Color("#ebdbb2"),
+            "hovered_outline": pygame.Color("#ebdbb2"),
+            "disabled_outline": pygame.Color("#a89984"),
+            "selected_outline": pygame.Color("#ebdbb2"),
+            "active_outline": pygame.Color("#ebdbb2"),
+        }
+    }
+}
+
+# Create a UIManager with the theme
+manager = pygame_gui.UIManager((WIDTH, HEIGHT), os.path.join(os.getcwd(), "theme.json"))
+# Define the doomsday is running function
+def doomsday_on():
+    global doomsday_running
+    doomsday_running = True
+# Define the doomsday function
+def doomsday():
+    # Clear the screen
+    screen.fill((0, 0, 0))
+
+    # Kill the existing buttons
+    top_secret_button.kill()
+    know_button.kill()
+
+    # Create a new loop that simulates a doomsday button
+    doomsday = True
+    countdown = 10
+
+    # Create the "DO NOT TOUCH" button
+    dont_touch_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH // 2 - 75, HEIGHT // 2 - 25), (150, 50)),
+                                                     text='DO NOT TOUCH',
+                                                     manager=manager)
+
+    # Create the "RETURN" button
+    global return_button
+    return_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH - 160, 10), (150, 50)),
+                                                 text='RETURN',
+                                                 manager=manager)
+
+    while doomsday:
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == return_button:
+                        # Kill the "DO NOT TOUCH" button
+                        dont_touch_button.kill()
+                        doomsday = False
+                        break
+                    elif event.ui_element == dont_touch_button:
+                        # Kill the "DO NOT TOUCH" button
+                        dont_touch_button.kill()
+                        # Start the countdown
+                        for i in range(countdown, 0, -1):
+                            for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                    pygame.quit()
+                                    sys.exit()
+                            screen.fill(theme["dark"]["colours"]["dark_bg"])
+                            font = pygame.font.SysFont(theme["dark"]["font"]["name"], 100)
+                            text = font.render(str(i), True, pygame.Color("red"))
+                            text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+                            screen.blit(text, text_rect)
+                            pygame.display.flip()
+                            pygame.time.delay(1000)
+                        # Display the "You died" message
+                        screen.fill(theme["dark"]["colours"]["dark_bg"])
+                        font = pygame.font.SysFont(theme["dark"]["font"]["name"], 100)
+                        text = font.render("You died", True, pygame.Color("red"))
+                        text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+                        screen.blit(text, text_rect)
+                        
+                        # Display the "I told you so" message
+                        font = pygame.font.SysFont(theme["dark"]["font"]["name"], 50)
+                        text = font.render("I told you so >:)", True, pygame.Color("red"))
+                        text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2 + 75))
+                        screen.blit(text, text_rect)
+                        
+                        pygame.display.flip()
+                        pygame.time.delay(2000)
+
+            # Move the process_events call inside the for loop
+            manager.process_events(event)
+
+        manager.update(time_delta)
+        manager.draw_ui(screen)
+
+        pygame.display.flip()
+
+    # Destroy the "DO NOT TOUCH" and "RETURN" buttons
+    return_button.kill()
+
+    # Draw the main buttons
+    draw_main()
+
+    # Reset the doomsday_running flag
+    doomsday_running = False
+
+    # Blit an image onto the screen after returning from doomsday
+    screen.blit(images[index], (0, 0))
+
+    # Return to the slideshow
+    start_gallery = True
+
+# Confirm both buttons are drawn
+draw_main()
+
 # Main loop
 index = 0
+last_change_time = pygame.time.get_ticks()
+change_interval = random.randint(3000, 4000) # 3-4 seconds
 while True:
     old_image = images[index]
     image_changed = False
+    time_delta = clock.tick(60)/1000.0
     for event in pygame.event.get():
-        action = handle_event(event)
-        if action == 'next':
-            index = (index + 1) % len(images)
-            image_changed = True
-        elif action == 'prev':
-            index = (index - 1 + len(images)) % len(images)
-            image_changed = True
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-    if image_changed:
-        fade_transition(old_image, images[index])
+        # Handle button trees
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == top_secret_button:
+                    doomsday()
+                elif event.ui_element == know_button:
+                    pass
+
+        manager.process_events(event)
+
+    # Change the photo if the time interval has elapsed
+    if pygame.time.get_ticks() - last_change_time > change_interval:
+        index = (index + 1) % len(images)
+        image_changed = True
+        last_change_time = pygame.time.get_ticks()
+        change_interval = random.randint(3000, 4000) # 3-4 seconds
 
     screen.blit(images[index], (0, 0))
+    if image_changed and not doomsday_running:
+        screen.blit(images[index], (0, 0))
+    if image_changed and doomsday_running:
+        screen.blit(images[index], (0, 0))
+
+    # Draw the buttons after drawing the images
+    manager.update(time_delta)
+    manager.draw_ui(screen)
+
     pygame.display.flip()
